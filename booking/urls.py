f9376b93826f2d1a45b94e1de7a45739cc73ccae
Mahsak89 <>
2023-08-02 16:26:08 +0000

    added navbar base.html style.css massages booking ability and page

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, BookingView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('booking/', BookingView.as_view(), name='booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -8,3 +8,47 @@ from django.urls import reverse_lazy
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+
+class BookingView(View):
+    template_name = "booking.html"
+    weekdays = []
+    validate_weekdays = []
+
+    def get(self, request):
+        today = datetime.now()
+        self.weekdays = self.validWeekday(22)
+        self.validate_weekdays = self.isWeekdayValid(self.weekdays)
+        return render(request, self.template_name, {
+            'weekdays': self.weekdays,
+            'validateWeekdays': self.validate_weekdays,
+        })
+
+    def post(self, request):
+        service = request.POST.get('service')
+        day = request.POST.get('day')
+        if not service:
+            messages.success(request, "Please Select A Service!")
+            return redirect('booking')
+
+        request.session['day'] = day
+        request.session['service'] = service
+        return redirect('bookingSubmit')
+
+    def validWeekday(self, days):
+        # Loop days you want in the next 21 days:
+        today = datetime.now()
+        weekdays = []
+        for i in range(0, days):
+            x = today + timedelta(days=i)
+            y = x.strftime('%A')
+            if y == 'Monday' or y == 'Saturday' or y == 'Wednesday':
+                weekdays.append(x.strftime('%Y-%m-%d'))
+        return weekdays
+
+    def isWeekdayValid(self, x):
+        validateWeekdays = []
+        for j in x:
+            if Appointment.objects.filter(day=j).count() < 10:
+                validateWeekdays.append(j)
+        return validateWeekdays
