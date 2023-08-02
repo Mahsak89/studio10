a93ccaf3bee1cdd5acd30173e063dd9a222fcd7c
Mahsak89 <>
2023-08-02 23:11:14 +0000

    added members views ,urls and templates

--- studio10/urls.py
+++ studio10/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("booking.urls")),  # Added
+    path('user/', include("django.contrib.auth.urls")),
+    path('user/', include("members.urls")),
 ]
