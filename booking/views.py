713ff61b7dda030448eea355557f8404145886a6
Mahsak89 <>
2023-08-02 00:34:28 +0000

    added index.html to view and templates and base.html

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, View, ListView, UpdateView
+from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
+from .models import Appointment
+from django.contrib import messages
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
