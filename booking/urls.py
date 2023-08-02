9a9efd204d759d0c3d4f2144009ce3a14c1e74d3
Mahsak89 <>
2023-08-02 18:40:57 +0000

    added the views and urls for the bookingsubmit

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IndexView, BookingView
+from .views import IndexView, BookingView, BookingSubmitView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('booking/', BookingView.as_view(), name='booking'),
+    path('booking/submit/', BookingSubmitView.as_view(), name='bookingSubmit'),
+
 ]
--- booking/views.py
+++ booking/views.py
@@ -52,3 +52,80 @@ class BookingView(View):
             if Appointment.objects.filter(day=j).count() < 10:
                 validateWeekdays.append(j)
         return validateWeekdays
+
+
+class BookingSubmitView(View):
+    template_name = "bookingSubmit.html"
+
+    def get(self, request):
+        user = request.user
+        times = [
+            "3 PM", "4 PM", "5 PM"
+        ]
+        today = datetime.now()
+        min_date = today.strftime('%Y-%m-%d')
+        max_date = (today + timedelta(days=21)).strftime('%Y-%m-%d')
+
+        day = request.session.get('day')
+        service = request.session.get('service')
+        hour = self.checkTime(times, day)
+
+        return render(request, self.template_name, {
+            'times': hour,
+        })
+
+    def post(self, request):
+        user = request.user
+        times = [
+            "3 PM", "4 PM", "5 PM"
+        ]
+        today = datetime.now()
+        min_date = today.strftime('%Y-%m-%d')
+        max_date = (today + timedelta(days=21)).strftime('%Y-%m-%d')
+
+        day = request.session.get('day')
+        service = request.session.get('service')
+        hour = self.checkTime(times, day)
+
+        if not service:
+            messages.success(request, "Please Select A Service!")
+            return redirect('bookingSubmit')
+        elif day < min_date or day > max_date:
+            messages.success(
+                request, "The Selected Date Isn't In The Correct Time Period!")
+            return redirect('bookingSubmit')
+
+        date = self.dayToWeekday(day)
+        if date not in ['Monday', 'Saturday', 'Wednesday']:
+            messages.success(request, "The Selected Date Is Incorrect")
+            return redirect('bookingSubmit')
+
+        if Appointment.objects.filter(day=day).count() >= 11:
+            messages.success(request, "The Selected Day Is Full!")
+            return redirect('bookingSubmit')
+
+        time = request.POST.get("time")
+        appointment_exists = Appointment.objects.filter(
+            day=day, time=time).exists()
+        if not appointment_exists or (appointment_exists and time == Appointment.objects.get(day=day).time):
+            Appointment.objects.create(
+                user=user, service=service, day=day, time=time)
+            messages.success(request, "Appointment Saved!")
+            return redirect('index')
+        else:
+            messages.success(
+                request, "The Selected Time Has Been Reserved Before!")
+            return redirect('bookingSubmit')
+
+    def checkTime(self, times, day):
+        # Only show the time of the day that has not been selected before:
+        x = []
+        for k in times:
+            if Appointment.objects.filter(day=day, time=k).count() < 1:
+                x.append(k)
+        return x
+
+    def dayToWeekday(self, x):
+        z = datetime.strptime(x, "%Y-%m-%d")
+        y = z.strftime('%A')
+        return y
